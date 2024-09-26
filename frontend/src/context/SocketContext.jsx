8805bc8b2b548b42ae6eb8b193e085566ae98230
anandasaikiacse <anandasaikiacse@gmail.com>
2024-09-27 04:23:52 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chatapp.onrender.com", {
+			const socket = io("https://chat-app-5i5o.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
